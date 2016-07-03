865a2a45f4d9a548377a49d66d2ef0a01b6d055f
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2016-07-03 10:04:23 -0300

    First commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'express'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/express-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'express'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/express-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'express'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/express-production'
+  }
+};
+
+module.exports = config[env];
