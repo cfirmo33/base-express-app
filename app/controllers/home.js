cc93a9745ddaea0d18282b7ecaba1017e118c4e9
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2016-07-03 10:39:50 -0300

    Removes Jade as dependency

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -8,9 +8,6 @@ module.exports = function (app) {
 
 router.get('/', function (req, res, next) {
   db.Article.findAll().then(function (articles) {
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
+    res.json(articles);
   });
 });
